3ca9540befd3c66067f993bcba03c0b640627097
kl <kl@intentus.de>
2025-03-20 14:32:23 +0100

    OT-1945 - (Wochen)Aufgabe 7 "Modularisierung"
    - Aufräumarbeiten

--- aufgabe/Aufgabe13.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    satz = input("Bitte einen Satz eingeben: ").strip()
-    wort_liste = satz.split()
-    woerter_anzahl = len(wort_liste)
-    print(f"Der Satz enthält {woerter_anzahl} Wörter.")
